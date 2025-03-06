de360dd856adec0db1f2de7a0ff0dbcd94450b6b
Василий Григорьев <wwwvg@outlook.com>
2025-03-06 14:17:03 +0300

    Add project files.

---
+++ EmployeeManagement/Models/Employee.cs
@@ -0,0 +1,19 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace EmployeeManagement.Models
+{
+    public partial class Employee : ObservableObject
+    {
+        [ObservableProperty]
+        private string? _name;
+
+        [ObservableProperty]
+        private string? _surname;
+
+        [ObservableProperty]
+        private int _age;
+
+        [ObservableProperty]
+        private double _salary;
+    }
+}
